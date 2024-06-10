b4f591d5b3c7cad1211859252c8f72bfe9b29f0f
Juliana <juliana.arrighi@samy.com>
2024-06-10 20:47:37 +0200

    Initial gem layout 0.1.0.pre

--- lib/emoji_ruby_parser.rb
+++ lib/emoji_ruby_parser.rb
@@ -2,7 +2,48 @@
 
 require_relative "emoji_ruby_parser/version"
 
+require "json"
+
+# This module provides methods for parsing and handling emoji characters.
+# It includes functionalities like extracting emojis from text and potentially
+# replacing them with their descriptions based on a provided emoji data source.
 module EmojiRubyParser
   class Error < StandardError; end
-  # Your code goes here...
+  module_function
+
+  EMOJI_REGEX = /\p{Emoji}+/u.freeze
+
+  # Check if there is an emoji in the text string
+  def emojis?(text)
+    EMOJI_REGEX.match?(text)
+  end
+
+  # Split each emoji string into an array of characters
+  def uniq_emojis(emojis_array)
+    result = []
+
+    emojis_array.each do |emojis|
+      result << emojis.chars
+    end
+    result.flatten.uniq
+  end
+
+  def replace_emoji_with_text(text)
+    return unless emojis?(text)
+
+    emojis_array = text.scan(::EmojiRubyParser::EMOJI_REGEX)
+
+    emojis = uniq_emojis(emojis_array)
+
+    emoji_json = JSON.parse(File.read(File.absolute_path(File.dirname(__FILE__) + '/data/emoji.json')))
+
+    parsed_text = text
+
+    emojis.each do |moji|
+      emoji_hash = emoji_json.find { |hash| hash["emoji"] == moji }
+
+      parsed_text = parsed_text.gsub(moji, " #{emoji_hash ? emoji_hash["description"] : " "} ")
+    end
+    parsed_text
+  end
 end
--- spec/emoji_ruby_parser_spec.rb
+++ spec/emoji_ruby_parser_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe EmojiRubyParser do
   it "has a version number" do
     expect(EmojiRubyParser::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
